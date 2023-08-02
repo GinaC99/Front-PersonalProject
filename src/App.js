f021c5be3da2d08c0aa6bddd499269e11a2e504e
Gina Cortes <gipac2@hotmail.com>
2023-08-02 18:58:32 -0500

    Create a loader

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 const App = () => {
  return(
    <>
-
     <h1>Binevenido mi nombre es  Gina Paola Cortes, para continuar necesita estar registrado</h1>
     <p>Para ingresar click <a href='/login'>aqui</a></p>
     <p>Para registrarse click <a href='/register'>aqui</a>
---
+++ src/Components/Loader/Loder.jsx
@@ -0,0 +1,7 @@
+import style from './loaderStyle.module.css';
+const Loader = () => {
+    return(
+    <div className={style.loader}></div>
+    );
+}
+export default Loader;
