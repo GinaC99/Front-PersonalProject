85c38941effb41d64f65a434d11ec970f5abcc06
Gina Cortes <gipac2@hotmail.com>
2023-08-01 22:09:32 -0500

    Home

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 const App = () => {
  return(
    <>
-    <h1>Binevenido este es el portafolio de Gina Paola Cortes, para continuar necesita estar registrado</h1>
+
+    <h1>Binevenido mi nombre es  Gina Paola Cortes, para continuar necesita estar registrado</h1>
     <p>Para ingresar click <a href='/login'>aqui</a></p>
     <p>Para registrarse click <a href='/register'>aqui</a>
     </p>
